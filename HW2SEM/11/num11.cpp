36d3a758f1fb9c0a8d58829080f1e9cfbf03134e
Kibercsmo <143355750+kibercsmo@users.noreply.github.com>
2025-10-07 23:55:48 +0300

    Delete HW2SEM directory

--- HW2SEM/11/num11.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main(){
-    setlocale(LC_ALL,"rus");
-    for (int i = 32; i <= 126; i++){
-        std::cout << i << " - " << (char)i << std::endl;
-    }
-    return 0;
-}
